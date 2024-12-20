999b2b3b62d8c690cdb199ecc7b552917ebb9904
LSB30 <lukinhaasantana777@gmail.com>
2024-12-20 11:58:37 -0300

    ⚡create TransactionEntity

---
+++ src/entities/transactionEntity.ts
@@ -0,0 +1,31 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "transactions_logs" })
+export class Transaction extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "varchar", unique: true, length: 16 })
+  cardNumber: string;
+
+  @Column({ type: "varchar", length: 5 })
+  expirationDate: string;
+
+  @Column({ type: "varchar", length: 3 })
+  cvv: string;
+
+  @Column({ type: "decimal", precision: 10, scale: 2 })
+  amount: number;
+
+  @Column({ type: "varchar", length: 50 })
+  password: string;
+
+  @Column({ type: "varchar", length: 50 })
+  marchantId: string;
+
+  @Column({ type: "varchar", length: 20 })
+  status: string;
+
+  @Column({ type: "varchar", length: 255 })
+  validationResult: string;
+}
